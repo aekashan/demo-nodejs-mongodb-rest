1e1102dfca44468237620ad09e3dde7731547b9b
akachan <akachan@adirecruitment.com>
2023-01-12 00:06:48 +0700

    edit route

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,13 @@
 var express = require('express');
-//var mongodb = require('../db');
+var mongodb = require('../db');
+
+// [{"id":"1001","name":"Node.js for Beginners","category":"Node","price":990},{"id":"1002","name":"React 101","category":"React","price":3990},{"id":"1003","name":"Getting started with MongoDB","category":"MongoDB","price":1990}]
 
 var router = express.Router();
 
-const products = [
-  {
-    id: '1001',
-    name: 'Node.js for Beginners',
-    category: 'Node',
-    price: 990
-  },
-  {
-    id: '1002',
-    name: 'React 101',
-    category: 'React',
-    price: 3990
-  },
-  {
-    id: '1003',
-    name: 'Getting started with MongoDB',
-    category: 'MongoDB',
-    price: 1990
-  }
-];
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.json(products);
+  mongodb.getVal(res);
 });
 
 router.post('/values', function(req, res) {
@@ -36,7 +18,7 @@ router.post('/values', function(req, res) {
     res.send(JSON.stringify({status: "error", value: "Value undefined"}));
     return
   }
-  //mongodb.sendVal(val, res);
+  mongodb.sendVal(val, res);
 });
 
 router.delete('/values/:id', function(req, res) {
@@ -47,7 +29,7 @@ router.delete('/values/:id', function(req, res) {
     res.send(JSON.stringify({status: "error", value: "UUID undefined"}));
     return
   }
-  //mongodb.delVal(uuid);
+  mongodb.delVal(uuid);
   res.send(JSON.stringify({status: "ok", value: uuid}));
 });
 
